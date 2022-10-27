a9b3884851a0edcc7df034f9a3f43dffb726e1f0
PROG-MohamedDwidar <dwidar.mohamed@icloud.com>
2022-10-27 22:52:42 +0200

    doctorine/dbal + user image nullable

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,12 +14,22 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        @section('menu')
-        @include('layouts.navigation')
-        @show
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
 
-        <div class="container">
-            @yield('content')
+            <!-- Page Heading -->
+            @if (isset($header))
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
     </body>
 </html>
